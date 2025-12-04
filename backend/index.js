05462eaf7ba7d28490dfd4ba7f5d97cea3dbe564
thannhf <thanhnguyen126agz@gmail.com>
2025-12-04 16:01:23 +0700

    first commit

---
+++ backend/index.js
@@ -0,0 +1,89 @@
+const port = 4000;
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const multer = require("multer");
+const path = require("path");
+const cors = require("cors");
+
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect("mongodb+srv://thanhnguyen126agz:thanhnguyen126agz@cluster0.wfdyjkb.mongodb.net/Ecommerce-Web?appName=Cluster0");
+
+app.get("/", (req, res) => {
+    res.send("Express App is running!");
+});
+
+const storage = multer.diskStorage({
+    destination: './upload/images',
+    filename: (req, file, cb) => {
+        return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`);
+    }
+});
+
+const upload = multer({storage:storage});
+app.use('/images', express.static('upload/images'));
+app.post("/upload", upload.single('product'), (req, res) => {
+    res.json({
+        success: 1,
+        image_url: `http://localhost:${port}/images/${req.file.filename}`
+    })
+});
+
+const Product = mongoose.model("Product", {
+    id: {
+        type: Number, 
+        required: true,
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    image: {
+        type: String,
+        required: true,
+    },
+    category: {
+        type: String,
+        required: true,
+    },
+    new_price: {
+        type: Number,
+        required: true
+    },
+    old_price: {
+        type: Number,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+    },
+    available: {
+        type: Boolean,
+        default: true,
+    },
+});
+
+app.post("/addproduct", async(req, res) => {
+    const product = new Product({
+        id: req.body.id,
+        name: req.body.name,
+        image: req.body.image,
+        category: req.body.category,
+        new_price: req.body.new_price,
+        old_price: req.body.old_price,
+        date: req.body.date,
+        available: req.body.available
+    })
+});
+
+app.listen(port, (error) => {
+    if(!error) {
+        console.log("Server is running on port " + port);
+    } else {
+        console.log("Error: " + error);
+    }
+});
